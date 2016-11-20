0640e65f286cb4c69cdbc3a28dc357907df97e2f
AleksandraPejcic <aleksandrapejcic82@yahoo.com>
2016-11-20 09:59:45 +0100

    Removed unused imports

--- Team.Management/src/main/java/com/teammanager/model/Clients.java
+++ Team.Management/src/main/java/com/teammanager/model/Clients.java
@@ -1,15 +1,10 @@
 package com.teammanager.model;
 
-import java.util.Set;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 @Entity
--- Team.Management/src/main/java/com/teammanager/model/Projects.java
+++ Team.Management/src/main/java/com/teammanager/model/Projects.java
@@ -1,7 +1,6 @@
 package com.teammanager.model;
 
 import java.util.Date;
-import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -9,9 +8,6 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
